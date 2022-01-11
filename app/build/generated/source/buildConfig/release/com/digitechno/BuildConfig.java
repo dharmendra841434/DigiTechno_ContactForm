b1bb5ce502508cb81b85e649bd260d50c1b13e7d
dharmendra841434 <dhk7283013741@gmail.com>
2022-01-11 16:42:46 +0530

    first commit

---
+++ app/build/generated/source/buildConfig/release/com/digitechno/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.digitechno;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.digitechno";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1004;
+  public static final String VERSION_NAME = "1.0";
+}
